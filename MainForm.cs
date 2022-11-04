ed3ace6baa462f7df5f7384cf6d1a7079e356c9c
kiwi <49212064+kiwi515@users.noreply.github.com>
2022-11-04 11:04:33 -0400

    Initial commit

---
+++ MainForm.cs
@@ -0,0 +1,10 @@
+namespace WSSE
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# WSSE
+ Save editor for Wii Sports (2006)
